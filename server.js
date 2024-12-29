671ee4c85b16d1921a99eb47050710d627251bed
developper237 <166755441+developper237@users.noreply.github.com>
2024-12-29 14:16:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connexion à MongoDB
-mongoose.connect('mongodb+srv://sergende695:sergeleprince237@facebookuser.zf45c.mongodb.net/admin?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://sergende695:sergeleprince237@facebookuser.zf45c.mongodb.net/logindata?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
